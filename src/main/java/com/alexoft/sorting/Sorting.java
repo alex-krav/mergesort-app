095d493de6eccb737598a84228b45968bfaab6ae
Oleksandr Kravchuk <oakravchuk@luxoft.com>
2020-05-18 20:20:45 +0300

    updated Sorting service to work with files

--- src/main/java/com/alexoft/sorting/Sorting.java
+++ src/main/java/com/alexoft/sorting/Sorting.java
@@ -1,6 +1,8 @@
 package com.alexoft.sorting;
 
-import com.alexoft.algo.MergeSort;
+import com.alexoft.algo.external.ExternalMergeSort;
+
+import java.io.File;
 
 /**
  * Interface that combines all the processing of sorting array,
@@ -12,14 +14,14 @@ public interface Sorting {
      * Adds a MergeSort implementation for later processing.
      * @param mergeSort MergeSort implementation object
      */
-    void add(MergeSort mergeSort);
+    void add(ExternalMergeSort mergeSort);
 
     /**
      * Iteratively applies sorting algorithms to an input array.
      * @param numbers integer array
      * @param asc boolean flag (true - ascending order, false - descending order)
      */
-    void process(int[] numbers, boolean asc);
+    void process(File numbers, boolean asc);
 
     /**
      * Sorts data array using selected sorting method.
@@ -27,5 +29,5 @@ public interface Sorting {
      * @param data integer array
      * @param asc boolean flag (true - ascending order, false - descending order)
      */
-    void apply(MergeSort sorter, int[] data, boolean asc);
+    void apply(ExternalMergeSort sorter, File data, boolean asc);
 }
--- src/main/java/com/alexoft/sorting/SortingImpl.java
+++ src/main/java/com/alexoft/sorting/SortingImpl.java
@@ -1,12 +1,17 @@
 package com.alexoft.sorting;
 
 import com.alexoft.algo.AlgoStats;
-import com.alexoft.algo.MergeSort;
-import com.alexoft.algo.MergeSortBase;
+import com.alexoft.algo.external.ExternalMergeSort;
+import com.alexoft.algo.external.ExternalMergeSortBase;
 import com.alexoft.log.Logger;
+import com.alexoft.log.ScreenLogger;
+import com.alexoft.log.TerminalLogger;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.alexoft.common.FileUtils.copy;
 
 /**
  * Implementation of Sorting interface
@@ -14,46 +19,40 @@ import java.util.List;
 public class SortingImpl implements Sorting {
 
     private List<AlgoStats> statResults = new ArrayList<>();
-    private List<MergeSort> sorters = new ArrayList<>();
-    private Logger consoleLog;
-    private Logger fileLog;
+    private List<ExternalMergeSort> sorters = new ArrayList<>();
+    private Logger terminalLog;
     private Logger screenLog;
+    private boolean lastSorter;
 
     @Override
-    public void add(MergeSort mergeSort) {
+    public void add(ExternalMergeSort mergeSort) {
         this.sorters.add(mergeSort);
     }
 
     /**
      * Method creates a copy of array before invoking new sort.
      * Output array is saved to file.
-     * @param data integer array
+     * @param inputFile integer array
      * @param asc boolean flag (true - ascending order, false - descending order)
      */
     @Override
-    public void process(int[] data, boolean asc) {
+    public void process(File inputFile, boolean asc) {
         statResults.clear();
         // log input data
-        fileLog.print("input", data);
-        consoleLog.print("input array", data);
-        screenLog.print("input array", data);
-        // copy data
-        int n = data.length;
-        int[] copy = new int[n];
+        terminalLog.print("input array", inputFile);
+        screenLog.print("input array", inputFile);
 
-        for (MergeSort sorter : sorters) {
-            System.arraycopy(data, 0, copy, 0, n);
+        lastSorter = false;
+        for (int i = 0; i < sorters.size(); ++i) {
+            if (i == sorters.size()-1)
+                lastSorter = true;
             // apply sorting to array
-            apply(sorter, copy, asc);
+            apply(sorters.get(i), inputFile, asc);
         }
-
-        // log output data
-        fileLog.print("output", copy);
-        screenLog.print("\noutput array", copy);
 
         // log stats for sorting methods
         for(AlgoStats stats : statResults) {
-            consoleLog.print(stats);
+            terminalLog.print(stats);
             screenLog.print(stats);
         }
     }
@@ -61,30 +60,33 @@ public class SortingImpl implements Sorting {
     /**
      * Method changes input array. Interim results and output array are logged.
      * @param sorter MergeSort implementation object
-     * @param data integer array
+     * @param originalFile integer array
      * @param asc boolean flag (true - ascending order, false - descending order)
      */
     @Override
-    public void apply(MergeSort sorter, int[] data, boolean asc) {
-        MergeSortBase baseSorter = (MergeSortBase) sorter;
-        // sort & log interim and final results
-        baseSorter.setLogger(consoleLog);
-        baseSorter.setAscending(asc);
-        baseSorter.sort(data);
+    public void apply(ExternalMergeSort sorter, File originalFile, boolean asc) {
+        File copyFile = copy(originalFile);
 
+        ExternalMergeSortBase baseSorter = (ExternalMergeSortBase) sorter;
+        // sort & log interim and final results
+        baseSorter.setLogger(terminalLog);
+        baseSorter.setAscending(asc);
+        baseSorter.sort(copyFile);
+
+        // log output data
+        if (lastSorter)
+            screenLog.print("\noutput array", copyFile);
+        else if (!copyFile.delete())
+            terminalLog.print("Couldn't remove copy of input file " + copyFile.getName());
         // log stats
         statResults.add(baseSorter.getStats());
     }
 
-    public void setConsoleLog(Logger consoleLog) {
-        this.consoleLog = consoleLog;
+    public void setTerminalLog(TerminalLogger terminalLog) {
+        this.terminalLog = terminalLog;
     }
 
-    public void setFileLog(Logger fileLog) {
-        this.fileLog = fileLog;
-    }
-
-    public void setScreenLog(Logger screenLog) {
+    public void setScreenLog(ScreenLogger screenLog) {
         this.screenLog = screenLog;
     }
 }
